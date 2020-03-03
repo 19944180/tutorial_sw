17877544fbd8bb985ffdd5bfc0ad586e3caf9fda
joubert.damien@yahoo.fr <joubert.damien@yahoo.fr>
2020-03-04 09:36:49 +1100

    Add random image

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Chaos Butterfly
 
 Chaos Butterfly creates a web server to generates pixel butterflies!
+ 
+## Install
+
+Run `pip install -r requirements.txt`.
--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 import bottle
 import render
+import random
 
 @bottle.route('/<width:int>/<height:int>')
 def image(width, height):
+    if width % 2 == 1:
+        width -= 1
+    if height % 2 == 1:
+        height -= 1
     pixels = [0] * (width * height)
-    # your code goes here
+    for x in range(0, width // 2, 1):
+        for y in range(0, height // 2, 1):
+            val = int(random.uniform(0, 255))
+            pixels[x + y * height] = val
+            pixels[x + (height - y -1) * height] = val
+            pixels[width - x - 1 + y * height] = val
+            pixels[width - x - 1 + (height - y - 1) * height] = val
     return render.image(width, height, pixels)
 
 bottle.run(host='localhost', port=8080)
